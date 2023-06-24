ba4c3ea2182169a04ea1b6cad38556a17e42e152
glebuben <glebenbuben@gmail.com>
2023-06-24 22:07:48 +0300

    supplemented solutions for issues, mentioned in pull request review

--- key_gen.sh
+++ key_gen.sh
@@ -2,7 +2,7 @@ if [[ ! -f "${PRIVATE_KEY_PATH:-jwtRS256.key}" ]]; then
   ssh-keygen -t rsa -b 4096 -m PEM -f ${PRIVATE_KEY_PATH:-jwtRS256.key} -N ${RS_PASSPHRASE:-""}
 fi
 
-if [[ -f "${PRIVATE_KEY_PATH:-jwtRS256.key}.pub" ]]; then
+if [[ ! -f "${PRIVATE_KEY_PATH:-jwtRS256.key}.pub" ]]; then
   openssl rsa -in ${PRIVATE_KEY_PATH:-jwtRS256.key} -pubout -outform PEM -out ${PRIVATE_KEY_PATH:-jwtRS256.key}.pub
 fi
 
