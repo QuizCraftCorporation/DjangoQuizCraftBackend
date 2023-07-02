cc35209db3b49aef3669cbc40d48fc513c07a451
zaqbez39me <korolov_k@mail.ru>
2023-07-02 18:02:14 +0300

    removed unnecessary print

--- quiz/views.py
+++ quiz/views.py
@@ -69,7 +69,6 @@ class QuizViewSet(ViewSet):
 
     def retrieve(self, request, pk=None, **kwargs):
         answer = True if request.query_params.get('answer') else False
-        print(answer)
         get_quiz_serializer = GetQuizSerializer(data={"quiz_id": pk})
         get_quiz_serializer.is_valid(raise_exception=True)
         quiz = Quiz.objects.get(pk=pk)
