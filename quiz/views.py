6da45b9da46885b98bc03978c4c4b826ec5cb063
zaqbez39me <korolov_k@mail.ru>
2023-07-02 17:22:24 +0300

    refactored views and urls

--- quiz/views.py
+++ quiz/views.py
@@ -1,65 +1,88 @@
+from django.contrib.auth.decorators import permission_required
 from django.http import JsonResponse
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from django.utils.decorators import method_decorator
+from rest_framework import status, mixins
+from rest_framework.decorators import action, permission_classes
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 # View class for registration
+from rest_framework.viewsets import ViewSet, GenericViewSet, ViewSetMixin
+
 from QuizGeneratorModel.quiz_craft_package.quiz_generator import QuizGenerator
 from quiz.models import Material, Quiz
 from quiz.serializers import QuizAnswersSerializer, QuizCreateSerializer, QuizSerializer, QuizSubmissionSerializer, \
-    GetQuizSerializer, QuizzesMeSerializer
-
-
-class QuizCreateView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        serializer = QuizCreateSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        materials = []
-        name = serializer.validated_data["quiz_name"]
-        quiz = Quiz.objects.create(name=name, creator=request.user)
-        for file in serializer.validated_data["files"]:
-            materials.append(Material.objects.create(name=serializer.validated_data["source_name"],
-                                                     file=file))
-            materials[-1].quiz_set.add(quiz)
-        quiz_gen = QuizGenerator(debug=False)
-        result = quiz_gen.create_questions_from_files([str(material.file.file) for material in materials])
-        quiz.add_questions(result)
-        return Response(QuizSerializer(quiz).data)
-
-
-class QuizView(APIView):
-
-    def get(self, request, id):
-        get_quiz_serializer = GetQuizSerializer(data={"quiz_id": id})
-        get_quiz_serializer.is_valid(raise_exception=True)
-        quiz = Quiz.objects.get(id=id)
-        if quiz.private and quiz.creator != request.user:
-            return JsonResponse({"error": "This quiz is private and cannot be accessed by you."},
-                                status=status.HTTP_403_FORBIDDEN)
-        return Response(QuizSerializer(quiz).data)
-
-
-class QuizViewMe(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        quizzes_me_serializer = QuizzesMeSerializer(request.user)
-        return Response(quizzes_me_serializer.data)
-
-
-class QuizAnswersView(APIView):
-    def get(self, request, id):
-        quiz = Quiz.objects.get(id=id)
-        return Response(QuizAnswersSerializer(quiz).data)
-
+    GetQuizSerializer, QuizMeSerializer
 
 class QuizEvaluate(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = QuizSubmissionSerializer(data=self.request.data, context={'user': request.user})
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.save())
+
+
+class QuizViewSet(ViewSet):
+    """
+    View set for working with Quiz model instances in database.
+    """
+
+    @permission_classes([IsAuthenticated])
+    def list(self, request):
+        """
+        Get list of quizzes for current user.
+        """
+        queryset = request.user.quizzes.filter(ready__exact=True)
+        serializer = QuizMeSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @permission_classes([IsAuthenticated])
+    def create(self, request):
+        """
+        Create new quiz using QuizGeneratorModel submodule.
+        """
+        if request.user.quizzes.filter(ready__exact=True):
+            return JsonResponse({"detail": "You have quiz already generating for you."},
+                                status=status.HTTP_403_FORBIDDEN)
+        serializer = QuizCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        materials = []
+        max_questions = serializer.validated_data.get("max_questions")
+        name = serializer.validated_data["quiz_name"]
+        quiz = Quiz.objects.create(name=name, creator=request.user)
+        for file in serializer.validated_data["files"]:
+            new_material = Material.objects.create(name=serializer.validated_data["source_name"],
+                                                   file=file)
+            new_material.quiz_set.add(quiz)
+            materials.append(new_material)
+        quiz_gen = QuizGenerator(debug=False)
+        if max_questions:
+            result = quiz_gen.create_questions_from_files([str(material.file.file) for material in materials],
+                                                          max_questions=max_questions)
+        else:
+            result = quiz_gen.create_questions_from_files([str(material.file.file) for material in materials])
+        quiz.add_questions(result)
+        return Response(QuizSerializer(quiz).data)
+
+    def retrieve(self, request, pk=None):
+        answer = True if self.kwargs.get('answer') else False
+        get_quiz_serializer = GetQuizSerializer(data={"quiz_id": pk})
+        get_quiz_serializer.is_valid(raise_exception=True)
+        quiz = Quiz.objects.get(pk=pk)
+        if quiz.private and quiz.creator != request.user:
+            return JsonResponse({"detail": "This quiz is private and cannot be accessed by you."},
+                                status=status.HTTP_403_FORBIDDEN)
+        if answer:
+            quiz_serializer = QuizAnswersSerializer(quiz)  # Serializer for answer request
+        else:
+            quiz_serializer = QuizSerializer(quiz)  # Serializer for simple quiz request
+        return Response(quiz_serializer.data)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def attempt(self, request, pk=None):
+        quiz = get_object_or_404(Quiz, pk=pk)
+        serializer = QuizSubmissionSerializer(data=self.request.data, context={'user': request.user, 'quiz': quiz})
         serializer.is_valid(raise_exception=True)
         return Response(serializer.save())
